f177adfad848a467b4a0ab4a931dca846dbb09d8
MicheleMorelli <michele.morelli@london.ac.uk>
2020-02-06 12:01:48 +0000

    WIP adding a config file to create the dropdown's options

---
+++ cfg.d/z_simply_advanced_searchbar.pl
@@ -0,0 +1,8 @@
+# You can specify the fields that appear in the Simply Advanced Dropdown menu in this file. 
+
+
+$c->{simply_advanced_searchbar_fields} = [qw/
+    title
+    creators_name
+    date
+/];
